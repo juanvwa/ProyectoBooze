e74ccb858d2a788daf1c0575c93295832e11bdf2
Oscar Eduardo Bolaños Ocampo <oebolanoso@unal.edu.co>
2020-07-17 23:34:37 -0500

    Update for test
    I try to organize your code Mr.

--- Proyecto.py
+++ Proyecto.py
@@ -1,11 +1,17 @@
 import random
-dic = {'1' : 'Camarada Vodka' , '2' : 'Sargento cerveza' , '3' : 'Capitan Aguardiente' , '4' : 'Mayor vino' , '5' : 'Coronel Ron' , '6' : 'Compadre Tequila' , '7' : 'General Whiskey' }
 
-contador = random.randint(1, 7)
+def status():
+    nombre = input('Ingrese el nombre suyo :')
+    dic = {'1' : 'Camarada Vodka',
+           '2' : 'Sargento cerveza',
+           '3' : 'Capitan Aguardiente',
+           '4' : 'Mayor vino',
+           '5' : 'Coronel Ron',
+           '6' : 'Compadre Tequila',
+           '7' : 'General Whiskey' }
+    estatus = dic.get(f'{random.randint(1, 7)}')
 
-estatus = dic.get(f'{contador}')
+    print(f'Saludos {nombre} su estatus es {estatus}' )
 
-nombre = input('Ingrese el nombre suyo :')
-
-print(f'Saludos {nombre} su estatus es {estatus}' )
-
+if __name__ == "__main__":
+    status()
