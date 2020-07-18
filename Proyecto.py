951f160399a8de8f2fafe0215e8e934efbaa2bab
Juan Sebastían Peláez Garzón <juanvwa@gmail.com>
2020-07-17 21:42:42 -0500

    Primera version del programa

---
+++ Proyecto.py
@@ -0,0 +1,13 @@
+import random
+dic = {'1' : 'Camarada Vodka' , '2' : 'Sargento cerveza' , '3' : 'Capitan Aguardiente' , '4' : 'Mayor vino' , '5' : 'Coronel Ron' , '6' : 'Almirante Tequila' , '7' : 'General Whiskey' }
+
+contador = random.randint(0, 8)
+
+
+
+estatus = dic.get(f'{contador}')
+
+nombre = input('Ingrese el nombre suyo :')
+
+print(f'Saludos {nombre} su estatus es {estatus}' )
+
