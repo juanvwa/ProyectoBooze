769250f4878cfbaae96764b12e4fc7a584f8fd3b
Juan Sebastían Peláez Garzón <juanvwa@gmail.com>
2020-07-17 22:29:47 -0500

    Cambio en la etiqueta Tequila

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,5 @@
 import random
-dic = {'1' : 'Camarada Vodka' , '2' : 'Sargento cerveza' , '3' : 'Capitan Aguardiente' , '4' : 'Mayor vino' , '5' : 'Coronel Ron' , '6' : 'Almirante Tequila' , '7' : 'General Whiskey' }
+dic = {'1' : 'Camarada Vodka' , '2' : 'Sargento cerveza' , '3' : 'Capitan Aguardiente' , '4' : 'Mayor vino' , '5' : 'Coronel Ron' , '6' : 'Compadre Tequila' , '7' : 'General Whiskey' }
 
 contador = random.randint(1, 7)
 
