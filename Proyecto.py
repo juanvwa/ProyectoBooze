7138548d4881b12f3e202edbb6ccd560d96584af
Juan Sebastían Peláez Garzón <juanvwa@gmail.com>
2020-08-23 12:35:54 -0500

    Se agregó una variación para ser usada cuando hay más de una persona.

--- Proyecto.py
+++ Proyecto.py
@@ -12,6 +12,12 @@ def status():
     estatus = dic.get(f'{random.randint(1, 7)}')
 
     print(f'Saludos {nombre} su estatus es {estatus}' )
+def grupo():
+    personas = int(input('Cuantas personas son:'))
+    i = 0
+    while i < personas:
+        status()
+        i += 1
 
 if __name__ == "__main__":
-    status()
+    grupo()
